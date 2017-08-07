e0a8b204052405671cfba765392e8ec2f95835bc
Kevin Lau <kevin.lau31@gmail.com>
2017-08-07 09:38:21 -0500

    Add instruction for allowing multiple windows

--- groupon-card-linked-deals.js
+++ groupon-card-linked-deals.js
@@ -3,6 +3,7 @@
 // Log into Groupon, and open "https://www.groupon.com/occasion/cash-back-deals"
 // Expand all deals until everything is shown
 // run in JavaScript Console
+// make sure to allow all popups for groupon.com
 
 var deals = document.querySelectorAll("#result-list figure > a");
 
